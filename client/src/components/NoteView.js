9091548aae95502817c5efe1793aec1005558cb3
Kaleb Allen <7kaleb29allen@gmail.com>
2019-03-26 09:27:18 -0600

    view page and edit

--- client/src/components/NoteView.js
+++ client/src/components/NoteView.js
@@ -1,11 +1,39 @@
 import React from 'react'
 import { connect, } from 'react-redux'
-import { Header, Card, Button, } from 'semantic-ui-react'
+import { Header, Table, Container, Button,  } from 'semantic-ui-react'
+import NoteForm from './NoteForm'
+import {Link, } from 'react-router-dom'
 
 
-const NoteView = ({ note = {} }) => (
-    <Header as='h3'>{ note.name}</Header>
-)
+class NoteView extends React.Component {
+    state = {showForm: false}
+
+    render() {
+        const {showForm} = this.state
+        const { note={} }= this.props
+
+        return(
+            <Container>
+                <Link to='/notes'> View Other Notes</Link>
+                <Button onClick={this.toggleForm}>
+                    { showForm ? 'Cancel' : 'Edit'}
+                </Button>
+                { showForm ?
+                    <NoteForm {...note} closeForm={this.toggleForm} />
+                    :
+                    <div>
+                        <Header as='h3'>{ note.name}</Header>    
+
+                    </div>
+                }
+
+            </Container>
+
+        )
+    }
+}
+
+
 
 const mapStateToProps = (state, props) => {
     return { note: state.notes.find( a => a.id === parseInt(props.match.params.id))}
